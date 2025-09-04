7dcfba7f79d7500eaafb25edee84e805ae9d46d9
AdhemiR <adhemir.1608@gmail.com>
2025-09-04 07:57:52 -0500

    Construcción de Formulario Registrar
    Continuación Construcción de Formulario Registrar

--- Project/AuthECClient/src/app/user/registration/registration.component.ts
+++ Project/AuthECClient/src/app/user/registration/registration.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registration.component.html',
   styles: ``
 })
 export class RegistrationComponent {
+  constructor(public formBuilder: FormBuilder){}
 
+  form = this.formBuilder.group({
+    fullName : [''],
+    email : [''],
+    password : [''],
+    confirmPassword : ['']
+  })
 }
